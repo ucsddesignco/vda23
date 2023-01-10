06c9a6f6e0bd47a4546ac8aea2d8279d9b24b462
Shawn Kim <sjk002@ucsd.edu>
2023-01-10 01:00:05 -0800

    favicon + vqa mentors section

--- src/components/modal/index.js
+++ src/components/modal/index.js
@@ -13,24 +13,24 @@ function Modal(props) {
     <div>
          {props.show && (
             <div className='modal-container'>
-                <div className="background" onClick={props.onHide}>
-
-                </div>
+                <div className="background" onClick={props.onHide}></div>
                 <div className="modal">
                     <div className={`top ${current_team.color}`}>
-                        <img className='profile' src={require(`../../assets/images/modal/${current_team.name}.png`)} alt="" />
-                        <h2>{current_team.name}</h2>
+                        <div className="profile-container">
+                            <img className='profile' src={require(`../../assets/images/modal/${current_team.name}.png`)} alt="" />
+                            <h2>{current_team.name}</h2>
+                        </div>
                         <img onClick={props.onHide} className='cross' src={Cross} alt="" />
                     </div>
-                    <div className="middle">
+                    <div className={`middle ${current_team.textSize}`}>
                         <h4>Has experience in</h4>
                         <p>{current_team.experience}</p>
                         <h4>Specialties</h4>
                         <p>{current_team.specialty}</p>
-                        <h4>Talk to her about</h4>
+                        <h4>{`Talk to ${current_team.gender} about`}</h4>
                         <p className='text-small'>{current_team.fun}</p>
                     </div>
-                    <img className={`symbol ${current_team.symbolStyling}`} src={require(`../../assets/images/program/symbol${current_team.symbol}.png`)} alt="" />
+                    <img className={`symbol ${current_team.imgSize}`} src={require(`../../assets/images/program/symbol${current_team.symbol}.png`)} alt="" />
                 </div>
             </div>
          )}
