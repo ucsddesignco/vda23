27f2214b8bcff63cad8734b6b348ffd362377bcf
brianliu1412 <briangliu1@gmail.com>
2023-04-13 18:12:04 -0700

    styled desktop images for experience page, touched up some styling for titles and headings

--- src/components/experience/index.js
+++ src/components/experience/index.js
@@ -20,8 +20,8 @@ function Experience() {
               <a target='_blank' href='https://thumbs.dreamstime.com/b/capybara-business-suit-animal-businessman-funny-boss-capybara-headed-man-formal-business-suit-generative-ai-illustration-268259284.jpg'>Read more about the VDA experience here</a>
             </div>
             <div id="bottom">
-              <p className="quote">“[VDA] helped me to actually do assignments that were interesting to me, [and I] learned how to do professional work outside of the basic hs work.”</p>
-              <h5 className="quote-name">Leyna Pham, 1st year</h5>
+              <p className="quote">“Coming into VDA, I had some experience with design but not on Figma and Illustrator. VDA allowed me to have the space to learn, experiment, and get feedback. Through the tutorials...resources, and Zoom calls with mentors, I felt like I was able to make tremendous progress and learn so much.”</p>
+              <h5 className="quote-name">Ellie Chen, 1st year</h5>
             </div>
           </div>
           <img alttext="Image of Students at a VDA Meeting" src={exp1} />
@@ -31,27 +31,21 @@ function Experience() {
         <img alttext="VDA Group Photo" className='section-2' src={exp2} />
         
         <div className='section-3'>
-          <div className="section-3-left">
-            {/* <img src={exp3} /> */}
-          </div>
+          <img alttext="Somi Somi Social" className="section-3-left" src={exp3} /> 
           <div className="section-3-right">
-            <div id="top">
-              {/* <img src={exp4} /> */}
-            </div>
+            <img alttext="Image of VDA Meeting" id="top" src={exp4} />
             <div id="bottom">
-              <p>“My biggest takeaway was definitely being guided through these applications. It helped me shorten my learning curve by a lot by being able to ask questions and learn from the tutorials.”</p>
-              <h5>Jeana Yoon, 2nd year</h5>
+              <p>“[VDA] helped me to actually do assignments that were interesting to me, [and I] learned how to do professional work outside of the basic hs work.”</p>
+              <h5>Leyna Pham, 1st year</h5>
             </div>  
           </div>
         </div>
         <div className="section-4">
           <div id='left'>
-            <p>“insert quote insert quote insert quote ”</p>
-            <h5 className='quote-name'>Insert name, insert year</h5>
+            <p>“My biggest takeaway was definitely being guided through these applications. It helped me shorten my learning curve by a lot by being able to ask questions and learn from the tutorials.”</p>
+            <h5 className='quote-name'>Jeana Yoon, 2nd year</h5>
           </div>
-          <div className="section-4-right">
-            {/* <img src={exp5} /> */}
-          </div>
+          <img alttext="Two VDA Students enjoying class." className="section-4-right" src={exp5} />
         </div>
         
         <div className="pinkbar" />
