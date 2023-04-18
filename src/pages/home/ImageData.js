f37415a00d46a797c32bf4a7689aa74701298351
Arth Shukla <44527268+arth-shukla@users.noreply.github.com>
2023-04-18 02:36:22 -0700

    Feat: add all (currently available) thumbnail data

---
+++ src/pages/home/ImageData.js
@@ -0,0 +1,233 @@
+import adahuynh from '../../assets/images/thumbnails/Ada_Huynh_F3.png'
+import alicelam from '../../assets/images/thumbnails/Alice_Lam_F3.png'
+import amyto from '../../assets/images/thumbnails/Amy_To_F3.png'
+import anyagommeren from '../../assets/images/thumbnails/Anya_Gommeren_F3.png'
+import arinakonnova from '../../assets/images/thumbnails/Arina_Konnova_F3.png'
+import beverlyto from '../../assets/images/thumbnails/Beverly_To_F3.png'
+import chauanhnguyen from '../../assets/images/thumbnails/ChauAnh_Nguyen_F3.png'
+import christineuy from '../../assets/images/thumbnails/Christine_Uy_F3.png'
+import cindynguyen from '../../assets/images/thumbnails/Cindy_Nguyen_F3.png'
+import doanduong from '../../assets/images/thumbnails/Doan_Duong_F3.png'
+import elliechen from '../../assets/images/thumbnails/Ellie_Chen_F3.png'
+import hillaryma from '../../assets/images/thumbnails/Hillary_Ma_F3.png'
+import irenejoo from '../../assets/images/thumbnails/Joo_Irene_F3.png'
+import jamelenedevera from '../../assets/images/thumbnails/Jamelene_Devera_F3.png'
+// import jeanayoon from '../../assets/images/thumbnails/Jeana_Yoon_F3.png'
+import joanaperalta from '../../assets/images/thumbnails/Joana_Peralta_F3.png'
+import justinalinas from '../../assets/images/thumbnails/Justin_Alinas_F3.png'
+import katiebonilla from '../../assets/images/thumbnails/Katie_Bonilla_F3.png'
+import kaylielam from '../../assets/images/thumbnails/Kaylie_Lam_F3.png'
+import keanunazemi from '../../assets/images/thumbnails/Keanu_Nazemi_F3.png'
+import leynapham from '../../assets/images/thumbnails/Leyna_Pham_F3.png'
+// import lindseyto from '../../assets/images/thumbnails/Lindsey_To_F3.png'
+import madewidyadhari from '../../assets/images/thumbnails/Made_Widyadhari_F3.png'
+import mayapatel from '../../assets/images/thumbnails/Maya_Patel_F3.png'
+import nicholasregli from '../../assets/images/thumbnails/Nicholas_Regli_F3.png'
+import nicolezhi from '../../assets/images/thumbnails/Nicole_Zhi_F3.png'
+import quynhmyphamnguyen from '../../assets/images/thumbnails/Quynh-my_Pham-Nguyen_F3.png'
+import rainasong from '../../assets/images/thumbnails/Raina_Song_F3.png'
+import sahilgathe from '../../assets/images/thumbnails/Sahil_Gathe_F3.png'
+import shelleynguyen from '../../assets/images/thumbnails/Shelley_Nguyen_F3.png'
+import sinduvipparthy from '../../assets/images/thumbnails/Sindu_Vipparthy_F3.png'
+import sonakshimohanty from '../../assets/images/thumbnails/Sonakshi_Mohanty_F3.png'
+import sophiezhu from '../../assets/images/thumbnails/Sophie_Zhu_F3.png'
+import susanduong from '../../assets/images/thumbnails/Susan_Duong_F3.png'
+import taylorkoshiol from '../../assets/images/thumbnails/Taylor_Koshiol_F3.png'
+import thidalen from '../../assets/images/thumbnails/Thida_Len_F3.png'
+import tinasohrabi from '../../assets/images/thumbnails/Tina_Sohrabi_F3.png'
+import winstonjeffries from '../../assets/images/thumbnails/Winston_Jeffries_F3.png'
+
+const ImageData = {
+	'Ada Huynh': {
+		src: adahuynh,
+		href: 'https://www.notion.so/F3-Final-Showcase-aa2fe2e004684a98a2a7441cc7d6b271?pvs=4',
+		alt: 'Submission from Ada Huynh.',
+	},
+	'Alice Lam': {
+		src: alicelam,
+		href: 'https://www.notion.so/Alice-s-VDA-Journey-8f0ad9915e7344d8b975a6fe575be4db?pvs=4',
+		alt: 'Submission from Alice Lam.',
+	},
+	'Amy To': {
+		src: amyto,
+		href: 'https://copper-syrup-63d.notion.site/F3-Amy-s-VDA-Showcase-Journal-9e7d779dd5714c2a8d0484489347843d',
+		alt: 'Submission from Amy To.',
+	},
+	'Anya Gommeren': {
+		src: anyagommeren,
+		href: 'https://best-pyroraptor-e97.notion.site/VDA-Final-Showcase-a5f01f4fb66743eb85d641a5d51f6e27',
+		alt: 'Submission from Anya Gommeren.',
+	},
+	'Arina Konnova': {
+		src: arinakonnova,
+		href: 'https://garrulous-cafe-7bb.notion.site/VDA-Journal-182122cb043944a28cdba67bc7d8c3aa',
+		alt: 'Submission from Arina Konnova.',
+	},
+	'Beverly To': {
+		src: beverlyto,
+		href: 'https://smoggy-pullover-0af.notion.site/My-Reflection-on-Design-Co-s-VDA-Program-6ae6be84d36e4028aeaa56ce4b6d4861',
+		alt: 'Submission from Beverly To.',
+	},
+	'Chau Anh Nguyen': {
+		src: chauanhnguyen,
+		href: 'https://cnvda.notion.site/Chau-Anh-Nguyen-s-VDA-Journal-bb9020f066f44bb680be9035cf1f2cf1',
+		alt: 'Submission from Chau Anh Nguyen.',
+	},
+	'Christine Uy': {
+		src: christineuy,
+		href: 'https://www.notion.so/VDA-F3-Journal-Submission-7e68ddbf22df48b591798b2b2accb00d?pvs=4',
+		alt: 'Submission from Christine Uy.',
+	},
+	'Cindy Nguyen': {
+		src: cindynguyen,
+		href: 'https://cindy-nguyen.notion.site/cindy-s-growth-journal-63d1145582f24478b24f31f5ea3dcc18',
+		alt: 'Submission from Cindy Nguyen.',
+	},
+	'Doan Duong': {
+		src: doanduong,
+		href: 'https://www.notion.so/Final-Showcase-4575d95a6c59411dadc4c28990c994ca?pvs=4',
+		alt: 'Submission from Doan Duong.',
+	},
+	'Ellie Chen': {
+		src: elliechen,
+		href: 'https://ellie-xinya.notion.site/Ellie-Chen-s-VDA-Journal-ae52e8462e6d43218682439a55512c61',
+		alt: 'Submission from Ellie Chen.',
+	},
+	'Hillary Ma': {
+		src: hillaryma,
+		href: 'https://hmeii.notion.site/VDA-Growth-Journal-d9c335f004ae462ea69d534e431f27d5',
+		alt: 'Submission from Hillary Ma.',
+	},
+	'Irene Joo': {
+		src: irenejoo,
+		href: 'https://irenejxx.notion.site/Irene-s-VDA-Growth-Journal-e036b6eb6e9e44f9bcaa94e829ea2ff9',
+		alt: 'Submission from Irene Joo.',
+	},
+	'Jamelene Devera': {
+		src: jamelenedevera,
+		href: 'https://www.notion.so/VDA-Showcase-Assignment-39c35c0376e24ce084df9c2a0793e250?pvs=4',
+		alt: 'Submission from Jamelene Devera.',
+	},
+	// 'Jeana Yoon': {
+	// 	src: jeanayoon,
+	// 	href: 'https://iced-tree-ac6.notion.site/F3-Final-Showcase-c01ed26810234156969b1105cab8089f',
+	// 	alt: 'Submission from Jeana Yoon.',
+	// },
+	'Joana Peralta': {
+		src: joanaperalta,
+		href: 'https://acute-spring-189.notion.site/All-Designs-Refections-2d3b6bdf57354351ba4bd8ba126ebf55',
+		alt: 'Submission from Joana Peralta.',
+	},
+	'Justin Alinas': {
+		src: justinalinas,
+		href: 'https://justinalinas.notion.site/Justin-Alinas-s-VDA-Journal-c816aadb0980454cb393484772a489b5',
+		alt: 'Submission from Justin Alinas.',
+	},
+	'Katie Bonilla': {
+		src: katiebonilla,
+		href: 'https://katiebonilla.notion.site/Katie-Bonilla-s-Journal-VDA-Program-Reflection-8188439c6dd64b57a11fd28f5a7e512f',
+		alt: 'Submission from Katie Bonilla.',
+	},
+	'Kaylie Lam': {
+		src: kaylielam,
+		href: 'https://vda-kaylielam.notion.site/b3e84cd456114f1aa913d58f4e1d7c17',
+		alt: 'Submission from Kaylie Lam.',
+	},
+	'Keanu Nazemi': {
+		src: keanunazemi,
+		href: 'https://splashy-zinc-2bc.notion.site/Visual-Design-Apprenticeship-c84c8c54cabf40ef8f97618193d7a8ab',
+		alt: 'Submission from Keanu Nazemi.',
+	},
+	'Leyna Pham': {
+		src: leynapham,
+		href: 'https://www.notion.so/Visual-Design-Apprenticeship-ada50f87e69c440c940697d4e7cfd710?pvs=4',
+		alt: 'Submission from Leyna Pham.',
+	},
+	'Made Widyadhari': {
+		src: madewidyadhari,
+		href: 'https://madewidyadhari.notion.site/Widya-s-awesome-vda-journal-2cb75fbe73f545b785bd317021d3ac30',
+		alt: 'Submission from Made Widyadhari.',
+	},
+	// 'Lindsey To': {
+	// 	src: lindseyto,
+	// 	href: 'https://lindseyto.notion.site/lindsey-s-vda-journey-98c777ea6c4647daa46f545c8050262e',
+	// 	alt: 'Submission from Lindsey To.',
+	// },
+	'Maya Patel': {
+		src: mayapatel,
+		href: 'https://foil-attempt-75e.notion.site/VDA-Journal-2f9c47c8909d462985ad5111b928ce06',
+		alt: 'Submission from Maya Patel.',
+	},
+	'Nicholas Regli': {
+		src: nicholasregli,
+		href: 'https://curious-apparel-382.notion.site/Nicholas-Regli-s-Design-Portfolio-5cb99c0fa6ec4e0280371eef1538af8e',
+		alt: 'Submission from Nicholas Regli.',
+	},
+	'Nicole Zhi': {
+		src: nicolezhi,
+		href: 'https://www.notion.so/VDA-final-assignment-growth-journal-8d3ca03312db4c9091d67df05ba0b4c5?pvs=4',
+		alt: 'Submission from Nicole Zhi.',
+	},
+	'Quynh-My Pham-Nguyen': {
+		src: quynhmyphamnguyen,
+		href: 'https://lying-asiago-90d.notion.site/VDA-Showcase-Quynh-My-Pham-e0b495887a6b483fbeb06b10e6eddf4b',
+		alt: 'Submission from Quynh-My Pham-Nguyen.',
+	},
+	'Raina Song': {
+		src: rainasong,
+		href: 'https://raina-song.notion.site/Raina-Song-VDA-Journal-0a7ad0d125c8497cb26b0fbf5ffd7377',
+		alt: 'Submission from Raina Song.',
+	},
+	'Sahil Gathe': {
+		src: sahilgathe,
+		href: 'https://cooing-plain-e13.notion.site/fc14b7d7b8414a2391a891502e54cfc0',
+		alt: 'Submission from Sahil Gathe.',
+	},
+	'Shelley Nguyen': {
+		src: shelleynguyen,
+		href: 'https://amazing-toast-1e1.notion.site/shelley-s-vda-journal-e97539eb92ff438faff492cf7cda76a0',
+		alt: 'Submission from Shelley Nguyen.',
+	},
+	'Sindu Vipparthy': {
+		src: sinduvipparthy,
+		href: 'https://delirious-crayfish-2bd.notion.site/sindu-vipparthy-s-vda-journal-805920d07c4a43a39a9d94f4b6ea7d4d',
+		alt: 'Submission from Sindu Vipparthy.',
+	},
+	'Sonakshi Mohanty': {
+		src: sonakshimohanty,
+		href: 'https://www.notion.so/Sonakshi-Mohanty-VDA-Journal-04d76f6306f14865895ab46622419a55?pvs=4',
+		alt: 'Submission from Sonakshi Mohanty.',
+	},
+	'Sophie Zhu': {
+		src: sophiezhu,
+		href: 'https://mellow-scabiosa-df6.notion.site/VDA-Showcase-Journal-TBD-efc3cd2967004f8684dc13018037f1fc',
+		alt: 'Submission from Sophie Zhu.',
+	},
+	'Susan Duong': {
+		src: susanduong,
+		href: 'https://liberating-edge-1d3.notion.site/Susan-Duong-s-VDA-Journal-327563d6a786498b89073f484fa36e09',
+		alt: 'Submission from Susan Duong.',
+	},
+	'Taylor Koshiol': {
+		src: taylorkoshiol,
+		href: 'https://sites.google.com/ucsd.edu/tkoshiolvda/home',
+		alt: 'Submission from Taylor Koshiol.',
+	},
+	'Thida Len': {
+		src: thidalen,
+		href: 'https://www.notion.so/VDA-Journal-f41a980cfd964007a138a687278bfcee?pvs=4',
+		alt: 'Submission from Thida Len.',
+	},
+	'Tina Sohrabi': {
+		src: tinasohrabi,
+		href: 'https://wooded-staircase-db9.notion.site/VDA-Journal-7c24999cb64f40d18776bfb9d07d713a',
+		alt: 'Submission from Tina Sohrabi.',
+	},
+	'Winston Jeffries': {
+		src: winstonjeffries,
+		href: 'https://incredible-value-aa0.notion.site/VDA-Journal-370ed755aa874b7183aabdb03969eabd',
+		alt: 'Submission from Winston Jeffries.',
+	},
+}
+
+export default ImageData
